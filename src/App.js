5c43dbab2777628d45d43dcb0adafaa81f82c181
alexfranco311 <alexfranco311@gmail.com>
2021-10-28 22:08:35 -0400

    before hamburger attempt

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
--- src/nav/navbar.js
+++ src/nav/navbar.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
+//const [navbarOpen, setNavbarOpen] = useState(false)
 
 function Navbar() {
     return (
         <nav className="navbar">
                 <div className="leftSide">
                     <div className="logo">
-                        <a href="/">Isla Sand</a>
+                        <a href="/">Sunny Codin' Iles</a>
                     </div>
                 </div>
             <div className="rightSide">
-                <div className="links">
+                <ul className="links">
                     <a href="/">Location</a>
                     <a href="/">Attractions</a>
                     <a href="/">Reviews</a>
-                </div>
+                </ul>
             </div>
                 
         </nav>
